29455fbc6cd7af7d5851043822eec551fe3646cd
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-03-10 14:54:00 -0600

    button playgrounds

--- src/app/course/courses.component.ts
+++ src/app/course/courses.component.ts
@@ -6,14 +6,31 @@ import { CoursesService } from './courses.service';
     <h2>{{ getTitle() }}</h2>
     <ul>
       <li *ngFor="let course of courses">
-        {{ course }}
+        {{ course }}<br />
+        <img src="{{ imageUrl }}" />
       </li>
     </ul>
+    <button [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button>
+    <button class="btn btn-primary" [class.active]="isActive">Save</button>
+    <div (click)="onDivClicked()">
+      <button (click)="onSave($event)">Save</button>
+    </div>
   `,
 })
 export class CoursesComponent {
+  isActive = false;
   title = 'List of courses';
+  imageUrl = 'http://lorempixel.com/400/200';
   courses;
+
+  onDivClicked() {
+    window.alert('Div was Clicked');
+  }
+
+  onSave($event: any) {
+    $event.stopPropagation();
+    console.log('Button was clicked', $event);
+  }
 
   constructor(service: CoursesService) {
     // Decouples the service
