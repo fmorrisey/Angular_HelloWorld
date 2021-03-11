b8ff861688468fe822717afa583cb7316808d598
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-03-11 14:48:47 -0600

    favorite feature

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,13 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.css'],
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class FavoriteComponent {
   @Input('isFavorite') public isFavorite: boolean;
-  @Output() change = new EventEmitter();
+  @Output('change') click = new EventEmitter();
 
   constructor() {
     this.isFavorite = false;
@@ -15,6 +22,9 @@ export class FavoriteComponent {
 
   clickStar() {
     this.isFavorite = !this.isFavorite;
-    this.change.emit();
+    this.click.emit({ newValue: this.isFavorite });
   }
+}
+export interface FavoriteChangedEventArgs {
+  eventState: boolean;
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'post',
   templateUrl: './post.component.html',
